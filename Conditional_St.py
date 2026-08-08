6cee307aa831dfb07114289dd8bd60c8b7c4d033
Divyansh Sharma <ds3741777@gmail.com>
2026-08-08 16:10:46 +0530

    Add file

---
+++ Conditional_St.py
@@ -0,0 +1,48 @@
+# fruits=18
+# if(fruits>=16):
+#     print("good")
+# elif(fruits==17):
+#     print("nice")
+# else:
+#     print("bad")
+
+# print("end of this code ")
+
+# Ques :- Grades Of Students On Their Marks :
+
+# marks=float(input("Enter marks of Student :"))
+# if(marks>=90):
+#     print("A")
+# elif(marks<90 and marks>80):
+#     print("B")
+# elif(marks>=60 and marks<80):
+#     print("C")
+# elif(marks<60 and marks>=33):
+#     print("D")
+# else:
+#     print("fail")
+
+#    Some Practise Questions :-
+# # 1)
+# num=int(input("enter any number :"))
+# if(num%2==0):
+#     print("iven no is even")
+# else :
+#     print("iven no is odd")
+
+# 2) Example of Nested Loops 
+a=int(input("enter 1st number :"))
+b=int(input("enter 2nd number :"))
+c=int(input("enter 3rd number :"))
+
+if(a>b):
+    if(a>c):
+        print("a is reater")
+
+elif(b>c):
+    print("b is reater")
+
+else:
+    print("c is reater")
+
+
