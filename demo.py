256e1c26fafaeaa2d78f3e86d3a8b7213e438af9
Divyansh Sharma <ds3741777@gmail.com>
2026-08-08 16:09:23 +0530

    Add file

---
+++ demo.py
@@ -0,0 +1,92 @@
+# # print("Hello, World!")
+# # print("divyans sarma ")
+
+# # print("Hello, World!","divyans sarma")
+
+# # print(35)
+# # print(8+4)
+
+# # variables :
+# Name="divy";
+# ummar=34;
+# name='divy';
+# name="divy";    # we can write a string in these format also .
+# name='''divy''';
+# print("my name is :",Name)
+# print("my ae is :",ummar)
+# print(name)
+# print(name)
+# print(name)
+
+# # datatypes :
+# print(type(Name))
+# print(type(ummar))
+
+# is_active = True
+# is_logged_in = False
+# a=None;
+
+# print(type(is_active))  # Output: <class 'bool'>
+# print(type(a))
+
+
+#Opeartors :
+#1). Aritmetic operators :
+# a=2
+# b=3
+# sum=a+b
+# diff=a-b
+# mul=a*b
+# div=a/b
+# div1=a//b      # witout fraction value
+# mod=a%b;
+# pow=a**b; 
+# print(sum)
+# print(diff)
+# print(mul)
+# print(div)
+# print(div1)   # point se pele wli value only 
+# print(mod) #remainder
+# print(pow)   #a^b
+
+# 2). Relational Operator
+# a=2
+# b=3
+# print(a!=b)
+# print(a==b)
+  
+# Type Casting :
+# a=3
+# b="4"
+# c=int(b)  
+#    or 
+# c=int("4")
+# print(type(c))
+# print(a+c)
+
+# input from user :
+# input("enter your name : ")
+# input()
+# int(input())
+
+# name=input("enter your name : ")
+# print("welcome",name)
+
+# Practise Some Questions :
+# 1)
+# a=int(input("Enter 1st number :"))
+# b=float(input("Enter 2nd number :"))
+# sum=a+b
+# print("sum of a and b is : " ,sum)
+# print(type(sum))
+
+# 2)
+# side=float(input("Enter the Side of Square :"))
+# print("Aera of Square is: ",side**2)
+
+# 3)
+a=int(input("Enter 1st number :"))
+b=int(input("Enter 2nd number :"))
+print(a>=b)
+
+
