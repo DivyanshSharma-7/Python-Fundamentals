a3d0ebf97ac21a0f5004453590740e62ae903b5f
Divyansh Sharma <ds3741777@gmail.com>
2026-08-08 16:10:28 +0530

    Add file

---
+++ String.py
@@ -0,0 +1,37 @@
+# a=input("emetr ")
+# print(len(a))
+
+# operations inn strins :
+# 1) Concatenation :
+# str1="Divy"
+# str2="sharma"
+# finalStr=str1+" "+str2
+# print(str1+str2)
+# print(finalStr)
+# print(len(finalStr))
+
+# 2) indexing :
+# str="Divy Sar,a"
+# print(str[8])
+
+# 3) Slicing :
+# a)
+# str="Leet Code"
+# print(str[0:3])
+# print(str[:3])
+# print(str[1:])
+# print(str[1:len(str)])
+
+# b) -ve indexing :
+# str="Apple"
+# print(str[-3])
+# print(str[-4])
+# print(str[-5:-1])
+
+# Some String Functions :-
+s="i am a coder."
+print(s.endswith("er."))
+print(s.capitalize())
+print(s.replace("a","D"))
+print(s.find("c"))
+print(s.count("c"))
