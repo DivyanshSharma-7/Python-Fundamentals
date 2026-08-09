6f4807d8edfc1f21521d3fb064082712a49acf01
Divyansh Sharma <ds3741777@gmail.com>
2026-08-09 14:26:28 +0530

    add file

---
+++ tuple.py
@@ -0,0 +1,28 @@
+# tuple is also like strin means Immutable .
+# tup=(1,)
+# print(tup)
+# print(type(tup))
+
+# tup=(1)
+# print(tup)
+# print(type(tup))    # now its type is int 
+
+
+# Tuple methods :
+# tup=(2,4,7,2,2)
+# print(tup.index(2))          # return index of 1st occurence of element i.e pass in (tup.index(element))
+# print(tup.count(2))             # count no of element in tuple i.e pass 
+
+# Some Practice Questions :
+# 1)
+rade=("A","B","A","D","A")
+print(rade.count("A"))
+
+rade=["A","B","A","D","A"]
+print(rade.sort())         # its not valid in list
+rade.sort()
+print(rade)
+
+
+
+
