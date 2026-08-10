922a9534241e8e57b6d78f166cc7f2e0b2eafbf9
Divyansh Sharma <ds3741777@gmail.com>
2026-08-10 23:48:28 +0530

    Add file

---
+++ sets.py
@@ -0,0 +1,48 @@
+# set is unordered & it Ignored duplicate value as you see Below !
+# setA={1,3,2,2,2,2,9,0,9,"leoo","fizz",3,1,"divy"}
+
+# print(setA)
+# print(type(setA))
+
+# print(len(setA)) # return total no. of items
+
+# **Empty Set** --> Syntax .    
+#    **it's not set={} because it is a empty Dictionary !
+# Null_Set=set()
+# print(Null_Set)
+# print(type(Null_Set))
+
+# **Methods In Set** :
+
+# collection={2,3,"divy",7}
+# collection.add(8)
+# collection.add((2,4,6))         # add tuple 
+# print(collection)
+
+# collection.remove("divy")
+# print(collection)
+
+# collection.clear()
+# print(collection)
+
+# newCollection={"leoo","fizz",3,1,"divy"}
+# print(newCollection.pop())
+# print(newCollection.pop())
+
+# set1={2,4,5,"siva","tv",9}
+# set2={1,3,5,"siva","lenovo",9}
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+
+# Some Practice Questions :
+# 1)
+# classroom={"pyton","java","cpp","pyton","javascript","java","java","cpp","c","pyton"}
+
+# print(classroom)
+# print(type(classroom))
+# print(len(classroom))
+
+# 2)
+set={("int",9),("float",9.0)}
+print(set)
